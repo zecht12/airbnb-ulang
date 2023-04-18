601676278a3d00b35bc38da66f07a8057d3c48f1
zecht12 <gilangprimae13@gmail.com>
2023-04-18 22:55:47 +0700

    push

---
+++ .history/components/navbar/Logo_20230418225539.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+    const router = useRouter();
+    
+    return ( 
+    <Image
+        onClick={() => router.push('/')}
+        className="hidden md:block cursor-pointer rounded-full w-[50px] h-[50px] hover:scale-110 "
+        src="/images/Logo.png"
+        height="50"
+        width="50"
+        alt="Logo"
+    />
+    );
+}
+
+export default Logo;
